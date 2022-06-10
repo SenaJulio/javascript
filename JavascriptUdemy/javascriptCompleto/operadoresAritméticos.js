ea2cabdbf2690ce57f0a758f0f12a526b2457678
JULIO SENA <gm.sena@hotmail.com>
2022-06-09 22:22:21 -0300

    MAIS ATUALIZAÇÕES

---
+++ JavascriptUdemy/javascriptCompleto/operadoresAritméticos.js
@@ -0,0 +1,18 @@
+/**
+ * Aritméticos
+ *  + Adição / concatenação
+ *  - / *  ** Potenciação
+ * 
+ * % resto da divisão
+ * ()
+ * **
+ * * / %
+ * + -
+ * incremento = ++
+ * Decremento = --
+ * Operadores de atribuição
+ */
+
+const num1 = 10;
+const num2 = 5;
+console.log(num1 / num2);
