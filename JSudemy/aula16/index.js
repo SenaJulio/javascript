2424fb8bbd699a8f99132170d9a2c86ff3b0d264
JULIO SENA <gm.sena@hotmail.com>
2022-07-16 21:38:39 -0300

    continuidade da aula

---
+++ JSudemy/aula16/index.js
@@ -0,0 +1,44 @@
+// indices        0     1         2    3    4
+const alunos =['Luiz','Maria', 'João']; // Luiza, Eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -2));
+
+//console.log(alunos[50]);
+
+// delete alunos[1];
+// console.log(alunos[1]);
+
+// const removido = alunos.pop();
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio');
+
+
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio');
+
+
+// alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Luiza'; Adicionando
+
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
