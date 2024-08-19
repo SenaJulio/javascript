b92875c6805f19abaac4cdf73c24947643e89c28
JULIO SENA <gm.sena@hotmail.com>
2024-08-19 18:16:45 -0300

    function

---
+++ JSudemy/funcao/index.js
@@ -0,0 +1,33 @@
+const raiz = (n) => {
+    return n ** 0.5;
+};
+
+
+//const raiz = function(n){
+//return n **0.5;
+//};
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+
+
+
+
+
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+console.log (soma(2, 2));
+console.log (soma(3, 1));
+console.log (soma(5, 10));
+
+
+function saudacao(nome){
+ return   `Bom dia ${nome}!`
+}
+const variavel = saudacao('Luiz');
+console.log(variavel);
